2cc877ce1e5260bd09ed7dafde67c631cba59db4
Yaerin Park <programmer94@naver.com>
2023-09-07 00:13:18 +0900

    Coin Tracker

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    // ★★★★★Challenge★★★★★
+    // input 생성 > 입력한 달러로 얼마의 코인을 살 수 있는지 계산해주는 앱을 만들어보자
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
+}
+
+export default App;
